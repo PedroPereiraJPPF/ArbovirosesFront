881c43a6d6297e56d4694ca053ec57d5ac791f5f
PedroPereiraJPPF <pedro.frutuoso@beedelivery.com.br>
2024-06-24 18:13:37 -0300

    fix

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -217,40 +217,38 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                   );
                 }}
               </SidebarLinkGroup>
-              <li>
-                {
-                  localStorage.getItem('token') != undefined && (
-                    <li>
-                      <NavLink
-                      to="/carregarDados"
-                      className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-                        pathname.includes('chart') && 'bg-graydark dark:bg-meta-4'
-                      }`}
-                    >
-                      <svg 
-                        className="fill-current"
-                        width="18"
-                        height="19"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192c0 35.3 28.7 64 64 64s64-28.7 64-64H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/>
-                        <defs>
-                          <clipPath id="clip0_130_9801">
-                            <rect
-                              width="18"
-                              height="18"
-                              fill="white"
-                              transform="translate(0 0.052124)"
-                            />
-                          </clipPath>
-                        </defs>
-                      </svg>
-                      Carregar Dados
-                      </NavLink>
-                    </li>
-                  )
-                }
-              </li>
+              {
+                localStorage.getItem('token') != undefined && (
+                  <li>
+                    <NavLink
+                    to="/carregarDados"
+                    className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
+                      pathname.includes('chart') && 'bg-graydark dark:bg-meta-4'
+                    }`}
+                  >
+                    <svg 
+                      className="fill-current"
+                      width="18"
+                      height="19"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                      <path d="M288 109.3V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V109.3l-73.4 73.4c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l128-128c12.5-12.5 32.8-12.5 45.3 0l128 128c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L288 109.3zM64 352H192c0 35.3 28.7 64 64 64s64-28.7 64-64H448c35.3 0 64 28.7 64 64v32c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V416c0-35.3 28.7-64 64-64zM432 456a24 24 0 1 0 0-48 24 24 0 1 0 0 48z"/>
+                      <defs>
+                        <clipPath id="clip0_130_9801">
+                          <rect
+                            width="18"
+                            height="18"
+                            fill="white"
+                            transform="translate(0 0.052124)"
+                          />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                    Carregar Dados
+                    </NavLink>
+                  </li>
+                )
+              }
             </ul> 
           </div> 
 
@@ -351,7 +349,6 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                                 </li>
                               )
                             }
-                          <li>
                             {
                               localStorage.getItem('token') != undefined && (
                                 <li>
@@ -367,7 +364,6 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
                                 </li>
                               )
                             }
-                          </li>
                         </ul>
                       </div>
                       {/* <!-- Dropdown Menu End --> */}
--- src/pages/Dashboard/LiraPage/index.tsx
+++ src/pages/Dashboard/LiraPage/index.tsx
@@ -1,13 +1,28 @@
+import { useState } from "react";
 import LineChart from "../../../components/Charts/Lira/LineChart";
 import LiraLineOptions from "../../../components/Charts/Lira/LineChart/options";
+import AgravoSelector from "../../../components/Forms/SelectGroup/AgravoSelector";
+import YearSelector from "../../../components/Forms/SelectGroup/YearSelector";
 import DefaultLayout from "../../../layout/DefaultLayout";
 
-const liraLineOptions = LiraLineOptions();
-
 const LiraPage: React.FC = () => {
+    const liraLineOptions = LiraLineOptions();
+    const [yearSelected, setYearSelected] = useState<string>((new Date().getFullYear()).toString())
+    const [agravoSelected, setAgravoSelected] = useState<string>('dengue')
+
     return (
         <DefaultLayout>
             <div className="mt-4 grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+            <div className='flex justify-end gap-x-2 items-center'>
+                <YearSelector
+                yearSelected={yearSelected}
+                setYearSelected={setYearSelected}
+                />
+                <AgravoSelector 
+                agravoSelected={agravoSelected}
+                setAgravoSelected={setAgravoSelected}
+                />
+            </div>
                 <LineChart 
                     options={liraLineOptions}
                     series={
