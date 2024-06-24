739173bbad9ebbc63287b9d09f3657470a1b59a6
PedroPereiraJPPF <pedro.frutuoso@beedelivery.com.br>
2024-06-24 18:01:38 -0300

    adiciona grafico de linhas para Lira

---
+++ src/components/Charts/Lira/LineChart/options.tsx
@@ -0,0 +1,111 @@
+import { ApexOptions } from "apexcharts";
+
+export default function LiraLineOptions() : ApexOptions
+{
+    const categories = Array.from({length: 53}, (_, index) => index + 1);
+    
+    return {
+        legend: {
+            show: true,
+            position: 'top',
+            horizontalAlign: 'left',
+        },
+        colors: ['#3C50E0', '#80CAEE', '#79C657'],
+        chart: {
+            fontFamily: 'Satoshi, sans-serif',
+            height: 335,
+            type: 'area',
+            dropShadow: {
+            enabled: true,
+            color: '#623CEA14',
+            top: 10,
+            blur: 4,
+            left: 0,
+            opacity: 0.1,
+            },
+            toolbar: {
+            show: true,
+            tools: {
+                download: true,
+                selection: true,
+                zoom: true,
+                zoomin: true,
+                zoomout: true,
+                pan: true,
+                reset: true,
+            }
+            },
+        },
+        responsive: [
+            {
+            breakpoint: 1024,
+            options: {
+                chart: {
+                height: 300,
+                },
+            },
+            },
+            {
+            breakpoint: 1366,
+            options: {
+                chart: {
+                height: 350,
+                },
+            },
+            },
+        ],
+        stroke: {
+            width: [2, 2],
+            curve: 'straight',
+        },
+        // labels: {
+        //   show: false,
+        //   position: "top",
+        // },
+        grid: {
+            xaxis: {
+            lines: {
+                show: true,
+            },
+            },
+            yaxis: {
+            lines: {
+                show: true,
+            },
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        markers: {
+            size: 4,
+            colors: '#fff',
+            strokeColors: ['#3056D3', '#80CAEE', '#5F9E41'],
+            strokeWidth: 3,
+            strokeOpacity: 0.9,
+            strokeDashArray: 0,
+            fillOpacity: 1,
+            discrete: [],
+            hover: {
+            size: undefined,
+            sizeOffset: 5,
+            },
+        },
+        xaxis: {
+            type: 'category',
+            categories: categories,
+            axisBorder: {
+            show: false,
+            },
+            axisTicks: {
+            show: false,
+            },
+            title: {
+            text: 'Indice de infestação predial (LIRA)',
+            style: {
+                fontSize: '16px',
+            },
+            }
+        }
+    }; 
+}
--- src/pages/Dashboard/LiraPage/index.tsx
+++ src/pages/Dashboard/LiraPage/index.tsx
@@ -1,10 +1,32 @@
+import LineChart from "../../../components/Charts/Lira/LineChart";
+import LiraLineOptions from "../../../components/Charts/Lira/LineChart/options";
 import DefaultLayout from "../../../layout/DefaultLayout";
+
+const liraLineOptions = LiraLineOptions();
 
 const LiraPage: React.FC = () => {
     return (
         <DefaultLayout>
-            <div>
-                Lira page
+            <div className="mt-4 grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+                <LineChart 
+                    options={liraLineOptions}
+                    series={
+                        [
+                            {
+                                name: "teste",
+                                data: [
+                                    20, 30, 10, 11, 5, 25
+                                ]
+                            },
+                            {
+                                name: "teste 2",
+                                data: [
+                                    40, 50, 30, 20, 40, 80
+                                ]
+                            }
+                        ]
+                    }
+                />
             </div>
         </DefaultLayout>
     );
